2e510a1164c6153db66355d08a89ddf8be40c30b
Jan Castermans <jan.castermans@pxl.be>
2025-08-22 14:14:03 +0200

    Quartz sync: Aug 22, 2025, 2:14 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -18,6 +18,9 @@ export default ((opts?: Options) => {
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
+            <li><a href="https://www.linkedin.com/school/hogeschoolpxl/"><img width="16" height="16" src='./static/linkedin.png'/></a></li>
+            <li><a href="https://www.facebook.com/HogeschoolPXL/"><img width="16" height="16" src='./static/facebook.png'/></a></li>
+            <li><a href="https://www.instagram.com/hogeschoolpxl"><img width="16" height="16" src='./static/instagram.png'/></a></li>
           {Object.entries(links).map(([text, link]) => (
             <li>
               <a href={link}>{text}</a>
