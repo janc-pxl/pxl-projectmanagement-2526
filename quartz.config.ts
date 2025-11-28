afcce30d8eec36c512067895b17b30a1ccf0f033
JanC <108794854+janc-pxl@users.noreply.github.com>
2025-11-28 14:09:19 +0100

    Comment out enablePopovers in quartz.config.ts
    Comment out the enablePopovers configuration option.

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Project Management",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    // enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
